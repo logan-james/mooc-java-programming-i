195075eeb041553da030f2ad061756582a857c65
logan-james <loganjames448@gmail.com>
2024-05-09 08:16:57 -0700

    Completed p2 of the challenge, I wasnt able to break out of the loop when the input scanner variable was outside the while loop, make sure its inside.

---
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class RepeatingBreakingAndRemembering {
+
+    public static void main(String[] args) {
+
+        // This exercise is worth five exercise points, and it is
+        // gradually extended part by part.
+
+        // If you want, you can send this exercise to the server
+        // when it's just partially done. In that case the server will complain about
+        // the parts you haven't done, but you'll get points for the finished parts.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give numbers:");
+        int sum = 0;
+
+        while (true) {
+
+            int input = Integer.valueOf(scanner.nextLine());
+
+            if (input == -1) {
+                break;
+            }
+
+            sum += input;
+
+        }
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+    }
+}
