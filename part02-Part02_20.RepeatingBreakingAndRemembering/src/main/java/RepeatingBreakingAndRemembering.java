18776e2e5b9006498a28b181bd6d9e42ded8c7fc
logan-james <loganjames448@gmail.com>
2024-05-09 08:27:40 -0700

    Learning when to use for loops vs while loops. 'When loop is predetermined number of time or number of repetitions can be based on user input the for loop is handy.'. Or 'When loop has to be executed until the user gives certain input the while-true loop is handy.'. This exercise highlights declaring variables outside loop along as the output from the loop outside instead of with the block of code when loop ends. Also ensuring the scanner variable is placed propperly.

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -15,6 +15,10 @@ public class RepeatingBreakingAndRemembering {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give numbers:");
         int sum = 0;
+        int numOfnums = 0;
+        double avg = 0;
+        int even = 0;
+        int odd = 0;
 
         while (true) {
 
@@ -24,10 +28,21 @@ public class RepeatingBreakingAndRemembering {
                 break;
             }
 
+            if (input % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
             sum += input;
+            numOfnums++;
 
         }
+        avg = 1.0 * sum / numOfnums;
         System.out.println("Thx! Bye!");
         System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + numOfnums);
+        System.out.println("Average: " + avg);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
     }
 }
