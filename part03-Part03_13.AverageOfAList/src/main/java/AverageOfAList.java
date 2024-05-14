e7b4552d0d2f3741a5f4fb041321dd078215c267
logan-james <loganjames448@gmail.com>
2024-05-14 16:24:41 -0700

    Creating ArrayList for user input, when -1 is entered while loop ended. For loop iterated through list adding up the sum and counting how many elements. Using sum and count of the list to calculate average, making the answer a double value.

---
+++ part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AverageOfAList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        // implement here a program, that first reads user input
+        // adding them on a list until user gives -1.
+        // Then it computes the average of the numbers on the list
+        // and prints it.
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+            list.add(input);
+        }
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < list.size(); i++) {
+            sum += list.get(i);
+            count++;
+        }
+        System.out.println("Average: " + ((double) sum / count));
+    }
+}
