dd5adf82b36b1c9d704c315248a5bfa0c9fc4a62
logan-james <loganjames448@gmail.com>
2024-05-16 12:07:10 -0700

    Again using array as a methods parameter. The personal highlight for this exercise was not including a comma with the last element of the array. You can see my old code commented out which wasnt working. The solution was to create a if statement for the anything less than the final element do this else do that. Instead I was creating an if statment for just the final element or else do that, basically the other way around.

---
+++ part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
@@ -0,0 +1,27 @@
+
+public class ArrayPrinter {
+
+    public static void main(String[] args) {
+        // You can test your method here
+        int[] array = { 5, 1, 3, 4, 2 };
+        printNeatly(array);
+    }
+
+    public static void printNeatly(int[] array) {
+        // Write some code in here
+        for (int i = 0; i < array.length; i++) {
+            if (i < array.length - 1) {
+                System.out.print(array[i] + ", ");
+            } else {
+                System.out.println(array[i]);
+            }
+        }
+        // for (int i = 0; i < array.length; i++) {
+        // if (array.length - 1 == array[i]) {
+        // System.out.print(array[i]);
+        // } else {
+        // System.out.print(array[i] + ", ");
+        // }
+        // }
+    }
+}
