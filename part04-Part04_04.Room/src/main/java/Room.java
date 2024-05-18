ba6227caa83b6e163b39551ddacec39b5c1579bb
logan-james <loganjames448@gmail.com>
2024-05-17 17:56:19 -0700

    Creating class and defining a Constructor. Custom objects are created the same way as objects from premade java classes, such as ArrayList, using the 'new' keyword. The constructor is defined after the instance variables, its defined in the class which then will be used to create a new __ object.

---
+++ part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,12 @@
+public class Room {
+
+  private String code;
+  private int seats;
+
+  // Constructor to initialize the variable
+  public Room(String classCode, int numberOfSeats) {
+    this.code = classCode;
+    this.seats = numberOfSeats;
+  }
+
+}
