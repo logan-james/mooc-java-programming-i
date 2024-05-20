aa44e3bc80ffac32d0934155d3f5b9569c36ad28
logan-james <loganjames448@gmail.com>
2024-05-20 13:15:32 -0700

    Practice defining methods for an object, creating classes, constructors, etc..

---
+++ part04-Part04_06.Door/src/main/java/Door.java
@@ -0,0 +1,13 @@
+public class Door {
+
+  // No instance variables for Door class
+
+  // Constructor, no parameters or variables
+  public Door() {
+
+  }
+
+  public void knock() {
+    System.out.println("Who's there?");
+  }
+}
