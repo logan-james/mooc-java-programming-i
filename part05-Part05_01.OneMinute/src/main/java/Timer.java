7826997dc70c48d9ee4561bdcc9bb4d33ebc9b7a
logan-james <loganjames448@gmail.com>
2024-05-28 19:11:00 -0700

    Using clock as a reference of how to use concept of Object Oriented Programming. Instead of a bunch of code with if statements and what not, make program more understandable  using Classes and Methods. OOP is about ISOLATING CONCEPTS into their own entities aka creating ABSTRACTIONS. In this exercise I created Timer class that uses clockhand objects to further be used in the main program, which is counting hundrendth of seconds then incremenets one second.. infinite.

---
+++ part05-Part05_01.OneMinute/src/main/java/Timer.java
@@ -0,0 +1,25 @@
+import java.time.Clock;
+
+public class Timer {
+
+  private ClockHand hundredth;
+  private ClockHand seconds;
+
+  public Timer() {
+    this.hundredth = new ClockHand(100);
+    this.seconds = new ClockHand(60);
+  }
+
+  public void advance() {
+    this.hundredth.advance();
+
+    if (this.hundredth.value() == 0) {
+      this.seconds.advance();
+
+    }
+  }
+
+  public String toString() {
+    return seconds + ":" + hundredth;
+  }
+}
