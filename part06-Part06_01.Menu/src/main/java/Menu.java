1507ec3a5c7ea353937e261f6f0f4c2ce1c71f52
logan-james <loganjames448@gmail.com>
2024-06-07 18:47:59 -0700

    Starting Part Six - Objects on a list and a list as part of an object.

---
+++ part06-Part06_01.Menu/src/main/java/Menu.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+
+public class Menu {
+
+    private ArrayList<String> meals;
+
+    public Menu() {
+        this.meals = new ArrayList<>();
+    }
+
+    // implement the required methods here
+    public void addMeal(String meal) {
+
+        // add meal to menu
+        // if meal already on list, don't add
+    }
+
+    // prints meals
+    public void printMeals() {
+
+    }
+
+    // clears menu
+    public void clearMenu() {
+
+    }
+}
