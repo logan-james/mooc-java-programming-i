4b18e166255ff1fa8cff4696d0fa511045603486
logan-james <loganjames448@gmail.com>
2024-06-20 13:22:25 -0700

    Finished Stack exercise

--- part06-Part06_02.Stack/src/main/java/Main.java
+++ part06-Part06_02.Stack/src/main/java/Main.java
@@ -10,5 +10,19 @@ public class Main {
         s.add("Value");
         System.out.println(s.isEmpty()); // false
         System.out.println(s.values()); // [Value]
+        String taken = s.take();
+        System.out.println(s.isEmpty()); // true
+        System.out.println(s.values()); // []
+        System.out.println(taken); // Value
+
+        s.add("1");
+        s.add("2");
+        s.add("3");
+        s.add("4");
+        s.add("5");
+
+        while (!s.isEmpty()) {
+            System.out.println(s.take());
+        }
     }
 }
